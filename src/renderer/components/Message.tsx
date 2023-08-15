e92ef35e0feae581ed4af4746aa93b4e9c98a9c8
Maulik Patel <maulikp163@gmail.com>
2023-08-15 18:30:48 +0530

    Added backgroud color for success in Message

--- src/renderer/components/Message.tsx
+++ src/renderer/components/Message.tsx
@@ -15,16 +15,22 @@ function Message({ signal, status }: { signal: Signal; status: string }) {
     ) {
       error = signal.value < lowLimit || signal.value > upLimit;
     }
-    // console.log(
-    //   `${signal.name} LOW: ${lowLimit}  UP: ${upLimit} Value: ${signal.value} Error: ${error}`
-    // );
+    console.log(
+      `${signal.name} LOW: ${lowLimit}  UP: ${upLimit} Value: ${signal.value} Error: ${error}`
+    );
   }
   const state = signal.states?.find((s) => s.value === signal.value);
   const phyValue =
     state?.state || (signal.value !== undefined ? signal.value : 'NULL');
 
   return (
-    <div className={`w-[16rem] m-2 p-2 border-2 ${error ? 'bg-red-800' : ''}`}>
+    <div className={`w-[16rem] m-2 p-2 border-2 ${
+            phyValue === 'NULL'
+              ? 'text-gray-500'
+              : error
+              ? 'text-yellow-200 bg-red-800'
+              : 'text-black bg-green-500'
+          }`}>
       <div className="flex flex-col m-0 p-0  w-full ">
         <span
           className="self-center font-semibold grow p-1"
@@ -32,15 +38,15 @@ function Message({ signal, status }: { signal: Signal; status: string }) {
         >
           {signal.name.replaceAll('_', ' ')}
         </span>
-        <Divider />
-        <span
-          className={`self-center p-1 font-bold ${
+        <Divider className={` ${
             phyValue === 'NULL'
-              ? 'text-gray-500'
+              ? 'bg-gray-800'
               : error
-              ? 'text-yellow-200'
-              : 'text-orange-500'
-          } `}
+              ? 'bg-red-500'
+              : 'bg-green-800'
+          }`}/>
+        <span
+          className={`self-center p-1 font-bold `}
         >
           {`${phyValue} ${
             state
